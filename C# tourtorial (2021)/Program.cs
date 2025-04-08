9f933e1d46c3cc1fa76e11204274a533b44992ba
Athenkosi Njamela <AthenkosiNj@mandela.ac.za>
2025-04-08 15:30:40 +0200

    Changes

--- C# tourtorial (2021)/Program.cs
+++ C# tourtorial (2021)/Program.cs
@@ -31,8 +31,8 @@ namespace C__tourtorial__2021_
 
             Console.WriteLine("Enter Name");
             string UserName = Console.ReadLine();
-            Console.WriteLine("Name: "+UserName);
-            Console.WriteLine("Age: "+Age);
+            Console.WriteLine("Name: " + UserName);
+            Console.WriteLine("Age: " + Age);
             Console.WriteLine("Age in 4 years: " + (Age + In4years)); //Age in 4 years
             Console.WriteLine();
 
@@ -47,23 +47,95 @@ namespace C__tourtorial__2021_
 
 
             double myDouble2 = 9.78;
-            int myInt2 = (int) myDouble2; //Explicit casting
+            int myInt2 = (int)myDouble2; //Explicit casting
 
             Console.WriteLine(myInt2);
             Console.WriteLine(myDouble2);
 
             Console.WriteLine(Convert.ToBoolean(myDouble2)); //Type Conversion Method 
 
-            //Operators
+            //Assignmet Operators
             int x = 100 + 5;
             Console.WriteLine(x);
+            x += 5; //Increment
+            Console.WriteLine(x);
 
+            //Comparison Operator
+            int y = 100;
+            Console.WriteLine(x <= y);
+
+            //Logical Operator
+            Console.WriteLine(x > 105 || y > 105);
+
+            //MATH
+            int a = 5, b = 10;
+
+            Math.Min(a, b);
+
+            Console.WriteLine(Math.Max(a, b));
+            Console.WriteLine(Math.Sqrt(a * b));
+            Console.WriteLine(Math.Abs(-5.10));
+            Console.WriteLine(Math.Round(9.99));
+
+            //String Interpolation
+
+            string firstName = "James";
+            string lastName = "Njamela";
+
+            Console.WriteLine();
+            string user = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(user);
+            Console.WriteLine();
+
+            //Access Strings
+
+            string myString = "Athenkosi";
+
+            Console.WriteLine(myString[0]); //looks for the the first index from the assigned string
+
+            Console.WriteLine(myString.IndexOf("e")); //looks for the position of "e" within the assigned string
+
+
+            //If...Else
+
+            int c = 10, d = 15;
+            bool outcome = false;
+
+            if (c == 10)
+            {
+                Console.WriteLine("Good");
+            }
+            
+            if (d < c)
+            {
+                Console.WriteLine("Okay");
+
+            }
+            else
+            {
+                Console.WriteLine(outcome);
+            }
+
+
+            if (c > d)
+            {
+                Console.WriteLine("okay");
+            }
+            else if (c < d)
+            {
+                Console.WriteLine("Okay");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
 
 
 
         }
-
+     
     }
+
 
 
 }
